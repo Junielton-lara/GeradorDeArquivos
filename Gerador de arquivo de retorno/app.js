51c03f9d4e41f48f5f0a31e1889e907bf09331cb
junielton-lara <junieltonlara01@gmail.com>
2022-01-25 16:49:17 -0300

    ajustes f

--- Gerador de arquivo de retorno/app.js
+++ Gerador de arquivo de retorno/app.js
@@ -1,178 +1,76 @@
-var pisFuncionarios = [];	
+var titulos, index;
 
-var nomePadrao = document.getElementById("nomePadrao").value;
-var departamento = document.getElementById("departamento").value;
-var dataAdmissao =  new Date(document.getElementById("dataAdmissao").value + 'T00:00');	
-var numFuncionarios = document.getElementById("numFuncionarios").value;
+
+function incluirTitulo(numeroDocumento, valorTitulo, valorPagoTitulo, DataVencimentoTitulo, ) {
+
+    titulos = document.getElementById("tbTitulos");
+    var tb = document.getElementById("tbTitulos");
+    var qtdLinhas = tb.rows.length;
+    var linha = tb.insertRow(qtdLinhas);
+
+    cellCodigo = linha.insertCell(0);
+    cellnumeroDocumento = linha.insertCell(1);
+    cellvalorTitulo = linha.insertCell(2);
+    cellvalorPagoTitulo = linha.insertCell(3);
+    cellDataVencimentoTitulo = linha.insertCell(4);
+
+    cellCodigo.innerHTML = qtdLinhas;
+    cellnumeroDocumento.innerHTML = numeroDocumento;
+    cellvalorTitulo.innerHTML = valorTitulo;
+    cellvalorPagoTitulo.innerHTML = valorPagoTitulo;
+    cellDataVencimentoTitulo.innerHTML = DataVencimentoTitulo;
+
+    totalLinhas = qtdLinhas;
+    // console.log("teste valor total de linhas =", totalLinhas);
+}
+
 var textToWrite = "";
 
-		function gerarFuncionarios()
-		{
-					
-			for(var i = 1; i <= numFuncionarios; i++ ) 
-			{
-				pisFuncionarios[i] = gerarPis();
-				textToWrite += nomePadrao + " " + i + ";" + pisFuncionarios[i] + ";" + departamento + ";" + dataAdmissao.toLocaleDateString() + "\r\n";   
-			}
-			
-			//var textToWrite = document.getElementById("inputTextToSave").value;
-			var textFileAsBlob = new Blob([textToWrite], {type:'text/plain'});
-			var fileNameToSaveAs = "ARQUIVO_RETORNO";
-			var downloadLink = document.createElement("a");
-			downloadLink.download = fileNameToSaveAs;
-			downloadLink.innerHTML = "Download File";
-			if (window.webkitURL != null)
-			{
-				// Chrome allows the link to be clicked
-				// without actually adding it to the DOM.
-				downloadLink.href = window.webkitURL.createObjectURL(textFileAsBlob);
-			}
-			else
-			{
-				// Firefox requires the link to be added to the DOM
-				// before it can be clicked.
-				downloadLink.href = window.URL.createObjectURL(textFileAsBlob);
-				downloadLink.onclick = destroyClickedElement;
-				downloadLink.style.display = "none";
-				document.body.appendChild(downloadLink);
-			}
 
-			downloadLink.click();
 
-		}
-		
-		function gerarPis() 
-		{
-			var n = 9;
-			var n1 = 1;//randomiza(1);
-			var n2 = 2;//randomiza(2);
-			var n3 = randomizaMaiorQueZero(5);
-			var n4 = randomiza(n);
-			var n5 = randomiza(n);
-			var n6 = randomiza(n);
-			var n7 = randomiza(n);
-			var n8 = randomiza(n);
-			var n9 = randomiza(n);
-			var n10 = randomiza(n);
-			//+ n10 * 4
-			var somatorio = (n1 * 3) + (n2 * 2) + (n3 * 9) + (n4 * 8) + (n5 * 7) + (n6 * 6) + (n7 * 5) + (n8 * 4) + (n9 * 3) + (n10 * 2);
-			var resto = (somatorio % 11);
-			var resultado = 0;
-			if (resto > 1)
-			{
-				resultado = 11 - resto;
-			}
-			numeroPuro = '' + n1 + n2 + n3 + n4 + n5 + n6 + n7 + n8 + n9 + n10 + resultado;
-			return numeroPuro;
-			//return '' + n1 + n2 + n3 + '.' + n4 + n5 + n6 + n7 + '.' + n8 + n9 + n10 + '-' + resultado;
-		}
-			
-		function randomiza(n) 
-		{
-			var ranNum = Math.round(Math.random() * n);
-			return ranNum;
-		}
+function gerarArquivo() {
 
-		function randomizaMaiorQueZero(n) 
-		{
-			var num = Math.round(Math.random() * n);
+    var dataCabecalho = document.getElementById("dataCabecalho").value;
+    var numeroDocumento = document.getElementById("numeroDocumento").value;
+    var valorTitulo = document.getElementById("valorTitulo").value;
+    var valorPagoTitulo = document.getElementById("valorPagoTitulo").value;
+    var DataVencimentoTitulo = document.getElementById("DataVencimentoTitulo").value;
 
-			if (num = 0) {
-				num = 1;
-			}
-			return num;
-		}
-				
-		function gerarMarcacao() 
-		{		
-			var dataInicio = moment.utc(document.getElementById("dataInicio").valueAsDate);
-			var dataFim = moment.utc(document.getElementById("dataFim").valueAsDate);
-			var dataInicioFormat = (dataInicio.date() < 10 ? "0" + dataInicio.date() : dataInicio.date()) + "" + ((dataInicio.month()+1) < 10 ? "0"+ (dataInicio.month() + 1) : (dataInicio.month() + 1)) + dataInicio.year().toString().substr(-4);
-			var dataFimFormat = (dataFim.date() < 10 ? "0" + dataFim.date() : dataFim.date()) + "" + ((dataFim.month()+1) < 10 ? "0"+ (dataFim.month() + 1) : (dataFim.month() + 1)) + dataFim.year().toString().substr(-4);
-			var diferenca = dataFim.diff(dataInicio, 'days');
-			var novaData;
-			var entrada1 = document.getElementById("entrada1").value;
-			entrada1 = entrada1.replace(":", "");
-			var saida1 = document.getElementById("saida1").value;
-			saida1 = saida1.replace(":", "");
-			var entrada2 = document.getElementById("entrada2").value;
-			entrada2 = entrada2.replace(":", "");
-			var saida2 = document.getElementById("saida2").value;
-			saida2 = saida2.replace(":", "");
-			var nsr = document.getElementById("nsr").value;
-			var cnpj = document.getElementById("cnpj").value;
-			var serieCompleta = document.getElementById("serieCompleta").value;
-			
-			var textToWrite = "02RETORNO01COBRANCA       00000000000005469645INTERBELLE COMERCIO DE PRODUTO237BRADESCO       0501220160000000022                                                                                                                                                                                                                                                                          050122         000001";
-			
-			for(var i = 1; i <= pisFuncionarios.length - 1; i++) 
-			{
-				novaData = moment(dataInicio);
 
-				for(var h = 0; h <= diferenca; h++) 
-				{
-					if(novaData.day() == 0 || novaData.day() == 6) 
-					{
-						console.log(novaData.day());
-					} 
-					else 
-					{
-						for(var j = 1; j <= 4; j++) 
-						{
-							if(j == 1) {
-								textToWrite += geraNsr(nsr++) + "3" + (novaData.date() < 10 ? "0" + novaData.date() : novaData.date()) + (novaData.month()+1 < 10 ? "0"+ (novaData.month() + 1) : (novaData.month() + 1)) + novaData.year().toString().substr(-4) + entrada1 + "0" + pisFuncionarios[i] + "\r\n";
-							} else if(j == 2) {
-								textToWrite += geraNsr(nsr++) + "3" + (novaData.date() < 10 ? "0" + novaData.date() : novaData.date()) + (novaData.month() +1 < 10 ? "0"+ (novaData.month() + 1) : (novaData.month() + 1)) + novaData.year().toString().substr(-4) + saida1 + "0" + pisFuncionarios[i] + "\r\n";
-							} 
-							else if (j == 3) {
-								textToWrite += geraNsr(nsr++) + "3" + (novaData.date() < 10 ? "0" + novaData.date() : novaData.date()) + (novaData.month() +1 < 10 ? "0"+ (novaData.month() + 1) : (novaData.month() + 1)) + novaData.year().toString().substr(-4) + entrada2 + "0" + pisFuncionarios[i] + "\r\n";
-							} 
-							else 
-							{
-								if(novaData.day() == 5) 
-								{
-									textToWrite += geraNsr(nsr++) + "3" + (novaData.date() < 10 ? "0" + novaData.date() : novaData.date()) + (novaData.month() +1 < 10 ? "0"+ (novaData.month() + 1) : (novaData.month() + 1)) + novaData.year().toString().substr(-4) + "1700" + "0"  + pisFuncionarios[i]+ "\r\n";
-								} 
-								else 
-								{
-									textToWrite += geraNsr(nsr++) + "3" + (novaData.date() < 10 ? "0" + novaData.date() : novaData.date()) + (novaData.month() +1 < 10 ? "0"+ (novaData.month() + 1) : (novaData.month() + 1)) + novaData.year().toString().substr(-4) + saida2 + "0"  + pisFuncionarios[i]+ "\r\n";
-								}		
-							}
-						}					
-					}
-					novaData.date(novaData.date() + 1);
-				}
-			}
+    var textToWrite = "02RETORNO01COBRANCA       00000000000005469645INTERBELLE COMERCIO DE PRODUTO237BRADESCO       " + dataCabecalho + "0160000000022                                                                                                                                                                                                                                                                          " + dataCabecalho + "         000001\r\n";
 
-			var textFileAsBlob = new Blob([textToWrite], {type:'text/plain'});
-			var fileNameToSaveAs = "ARQUIVO_RETORNO";
-			var downloadLink = document.createElement("a");
-			downloadLink.download = fileNameToSaveAs;
-			downloadLink.innerHTML = "Download File";
-			if (window.webkitURL != null)
-			{
-				// Chrome allows the link to be clicked
-				// without actually adding it to the DOM.
-				downloadLink.href = window.webkitURL.createObjectURL(textFileAsBlob);
-			}
-			else
-			{
-				// Firefox requires the link to be added to the DOM
-				// before it can be clicked.
-				downloadLink.href = window.URL.createObjectURL(textFileAsBlob);
-				downloadLink.onclick = destroyClickedElement;
-				downloadLink.style.display = "none";
-				document.body.appendChild(downloadLink);
-			}
+    for (var i = 1; i <= totalLinhas; i++) {
+        var nsr = i + 1;
+        textToWrite += "102111370510001860000009030990003965900000000000000000" + numeroDocumento + "00000000000" + numeroDocumento + "0000000000000000000000000090605012200" + numeroDocumento + "00000000000" + numeroDocumento + "0" + DataVencimentoTitulo + "" + valorTitulo + "23704151  000000000000000000000000000000000000000000000000000000000000000000000000000000" + valorPagoTitulo + "00000000000000000000000000   050122             00000000000000                                                                  00000" + nsr + "\r\n";
+    }
 
-			downloadLink.click();
-		}
-			
-		function geraNsr(nsr) 
-		{
-			nsr = nsr.toString();
-			while(nsr.length < 9) {
-				nsr = "0" + nsr;
-			}
-			return nsr;
-		}
+
+
+
+
+    for (var i = 1; i <= 1; i++) {
+        var nsrFinal = nsr + 1;
+        textToWrite += "9201237          000000020000000002608600000420          00000000000000000000000026086000020008458914800000200000002608600000000000000000000000000000000000000000000000000000000000000000000                                                                                                                                                                              00000000000000000000000         00000" + nsrFinal + "\r\n";
+    }
+
+
+    var textFileAsBlob = new Blob([textToWrite], { type: 'text/plain' });
+    var fileNameToSaveAs = "H_CB0501202201BPB";
+    var downloadLink = document.createElement("a");
+    downloadLink.download = fileNameToSaveAs;
+    downloadLink.innerHTML = "Download File";
+    if (window.webkitURL != null) {
+        // Chrome allows the link to be clicked
+        // without actually adding it to the DOM.
+        downloadLink.href = window.webkitURL.createObjectURL(textFileAsBlob);
+    } else {
+        // Firefox requires the link to be added to the DOM
+        // before it can be clicked.
+        downloadLink.href = window.URL.createObjectURL(textFileAsBlob);
+        downloadLink.onclick = destroyClickedElement;
+        downloadLink.style.display = "none";
+        document.body.appendChild(downloadLink);
+    }
+
+    downloadLink.click();
+}
